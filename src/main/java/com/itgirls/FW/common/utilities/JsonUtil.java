4b6a29ed70547843eb5bfa854765e43d74a510f8
Nancy Nguyen <ngannguyenqn2001@gmail.com>
2023-03-01 10:19:27 +0700

    add dataprovider

--- src/main/java/com/itgirls/FW/common/utilities/JsonUtil.java
+++ src/main/java/com/itgirls/FW/common/utilities/JsonUtil.java
@@ -1,4 +1,64 @@
 package com.itgirls.FW.common.utilities;
 
-public class JsonUtil {
+
+import com.itgirls.FW.common.AbstractLog;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+
+public class JsonUtil extends AbstractLog {
+    @RuntimeType
+    private static Method findMethod(Class testClass, String testcaseName) {
+        Method[] methods = testClass.getDeclaredMethods();
+        for (int i = 0; i < methods.length; i++) {
+            Method method = methods[i];
+            if (testcaseName.equals(method.getName()))
+                return method;
+        }
+        return null;
+    }
+
+    private static int getParameterCount(Method method) {
+        if (method != null) {
+            return method.getParameterCount();
+        }
+        return 0;
+    }
+
+    public static Object[][] getJSONTestData(String testcaseName, Class testClass) {
+        Method method = findMethod(testClass, testcaseName);
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsonObject = new JSONObject();
+
+        try {
+            Object obj = jsonParser.parse(new FileReader("src/main/resources/testdata/data.json"));
+            jsonObject = (JSONObject) obj;
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        JSONArray testDataList = (JSONArray) jsonObject.get(testcaseName);
+
+        // Get number of parameter of particular test method
+        int paramCount = getParameterCount(method);
+        Object[][] data = new Object[testDataList.size()][paramCount];
+
+        for (int i = 0; i < testDataList.size(); i++) {
+            JSONObject testData = (JSONObject) testDataList.get(i);
+            data[i] = new Object[paramCount];
+            for (int j = 0; j < paramCount; j++) {
+                Parameter p = method.getParameters()[j];
+                data[i][j] = testData.get(p.getName());
+            }
+        }
+        return data;
+
+    }
 }
--- src/test/java/com/itgirls/testsuite/LoginTest.java
+++ src/test/java/com/itgirls/testsuite/LoginTest.java
@@ -2,9 +2,11 @@ package com.itgirls.testsuite;
 
 import com.itgirls.FW.common.Constants;
 import com.itgirls.FW.common.utilities.ConfigUtil;
+import com.itgirls.FW.common.utilities.JsonUtil;
 import com.itgirls.FW.pages.LoginPage;
 import com.itgirls.testsuite.base.BaseTest;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static com.itgirls.FW.common.PageManager.getLoginPage;
@@ -12,12 +14,17 @@ import static com.itgirls.FW.common.PageManager.getProductsPage;
 
 public class LoginTest extends BaseTest {
 
-    @Test
-    public void loginWithValidInfo() {
+    @DataProvider(name = "loginValidData")
+    public Object[][] loginValidData() {
+        return JsonUtil.getJSONTestData("loginWithValidInfo", this.getClass());
+    }
+
+    @Test(dataProvider = "loginValidData")
+    public void loginWithValidInfo(String username, String password) {
         LoginPage loginPage = getLoginPage();
         loginPage.navigateTo(ConfigUtil.getPropertiesValue(Constants.URL));
-        loginPage.inputUsername("standard_user");
-        loginPage.inputPassword("secret_sauce");
+        loginPage.inputUsername(username);
+        loginPage.inputPassword(password);
         loginPage.clickLoginButton();
         Assert.assertTrue(getProductsPage().isProductsTitleDisplayed());
     }
