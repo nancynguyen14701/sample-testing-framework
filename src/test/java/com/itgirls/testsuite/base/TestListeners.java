a944b66318e2e5b1eb61eb33f094d918b72531a0
Nancy Nguyen <ngannguyenqn2001@gmail.com>
2023-03-05 14:28:47 +0700

    add alure report

--- src/test/java/com/itgirls/testsuite/base/TestListeners.java
+++ src/test/java/com/itgirls/testsuite/base/TestListeners.java
@@ -1,7 +1,6 @@
 package com.itgirls.testsuite.base;
 
 import com.itgirls.FW.common.AbstractLog;
-import com.itgirls.FW.common.DriverFactory;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
@@ -9,7 +8,6 @@ public class TestListeners extends AbstractLog implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        DriverFactory.quitDriver();
         logger.info("Failure listener implemented ");
     }
 }
